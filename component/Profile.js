44dec63316c7248ab077e521bf0e10210975c452
amroalbarham <amroalbarham@gmail.com>
2021-05-30 15:30:46 +0300

    asdf1

--- component/Profile.js
+++ component/Profile.js
@@ -3,8 +3,18 @@ import { withAuth0 } from '@auth0/auth0-react';
 
 class Profile extends Component {
   render() {
-    const { user } = this.props.auth0;
-    return <div>Hello {user.name}</div>;
+    const { user,isAuthenticated } = this.props.auth0;
+    return (
+        <>
+            { isAuthenticated &&
+                <>
+                    <div>Hello {user.name}</div>
+                    <div>Email: {user.email}</div>
+                </>
+
+            }
+        </>
+    )
   }
 }
 
--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import IsLoadingAndError from './IsLoadingAndError';
 import Footer from './Footer';
+import Login from './login';
+import { useAuth0 } from '@auth0/auth0-react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -21,6 +23,7 @@ class App extends React.Component {
               <Switch>
                 <Route exact path="/">
                   {/* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
+                  <Login/>
                 </Route>
                 {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
               </Switch>
